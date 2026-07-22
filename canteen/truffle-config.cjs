2dfcbbcc816294285232c2f63c38772ef37650d5
shivv23 <shivamkumar0423@gmail.com>
2026-07-22 15:06:49 +0530

    fix: replace web3.min.js with CommonJS re-export, use 127.0.0.1 for ganache
    Signed-off-by: shivv23 <shivamkumar0423@gmail.com>

--- canteen/truffle-config.cjs
+++ canteen/truffle-config.cjs
@@ -4,7 +4,7 @@ const HDWalletProvider = require('@truffle/hdwallet-provider');
 module.exports = {
   networks: {
     development: {
-      host: "localhost",
+      host: "127.0.0.1",
       port: 8545,
       gas: 6721975,
       network_id: "*" // Match any network id
